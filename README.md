15a7718f49123a06dd92c0a9719b484b50e16a7b
Nicolas Roos <nicolas.k.roos@gmail.com>
2021-06-14 09:37:21 +0200

    feat(app): basic implementation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # extra-extra
+A small app that checks for new articles on we.publish cms (and tsri.ch).
+Once a new article is found it will be sent to a Slack channel.
+
+## Usage
+* Create Slack App and setup an incoming Webhook
+* Create a file `app.yaml` and add the following:
+```yaml
+runtime: nodejs14
+env_variables:
+  SLACK_INCOMING_WEBHOOK: "replace with your incoming webhook"
+```
+* Deploy the app to Google App Engine
---
+++ index.js
@@ -0,0 +1,85 @@
+const { createClient } = require('@urql/core')
+const fetch = require('node-fetch')
+const Parser = require('rss-parser');
+const { IncomingWebhook } = require('@slack/webhook');
+
+const parser = new Parser();
+const incomingWebhook = process.env.SLACK_INCOMING_WEBHOOK
+console.log('webhook', incomingWebhook)
+const webhook = new IncomingWebhook(incomingWebhook);
+
+const bajourClient = createClient({
+    url: 'https://api.bajour.ch',
+    fetch
+});
+
+const kultzClient = createClient({
+    url: 'https://api.kultz.ch',
+    fetch
+});
+
+const QUERY = `
+query Test {
+    articles(first: 5) { 
+        nodes { 
+            publishedAt
+            title
+            url 
+        }
+    }
+}
+`
+
+const MILLISECS_SINCE_LAST_CHECK = 900000 // 15 min
+
+function handleGraphQLResponse(res) {
+    const {data} = res
+    if(data !== null && data.articles && data.articles.nodes) {
+        data.articles.nodes.forEach(article => {
+            const publishDate = new Date(article.publishedAt)
+            const lastCheckDate = new Date(new Date().getTime() - MILLISECS_SINCE_LAST_CHECK)
+            if(publishDate > lastCheckDate) {
+                console.log('new article', article)
+                printToSlackChannel(article.title, article.url, article.url.includes('kultz') ? 'Kultz.ch' : 'Bajour.ch')
+            }
+        })
+    }
+}
+
+function printToSlackChannel(title, link, newsroom) {
+ webhook.send({
+    text: `Neuer Artikel von ${newsroom} mit dem Titel "${title}" wurde soeben publiziert. ${link}`
+ })
+}
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', async (req, res) => {
+    const tsriRes = await parser.parseURL('https://tsri.ch/feed/')
+    const {items} = tsriRes
+    if(items) {
+        items.forEach((article) => {
+            const publishDate = new Date(article.pubDate)
+            const lastCheckDate = new Date(new Date().getTime() - MILLISECS_SINCE_LAST_CHECK)
+            if(publishDate > lastCheckDate) {
+                console.log('new article', article)
+                printToSlackChannel(article.title, article.link, 'Tsüri.ch')
+            }
+        })
+    }
+    const bajourItems = await bajourClient.query(QUERY, {}).toPromise()
+    handleGraphQLResponse(bajourItems)
+
+    const kultzItems = await kultzClient.query(QUERY, {}).toPromise()
+    handleGraphQLResponse(kultzItems)
+
+    res.send('DONE DONE!');
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
